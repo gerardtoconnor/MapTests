482d9785d11f4013b3e1287394c959f9f860871e
gerardtoconnor <gerardtoconnor@gmail.com>
2017-10-22 17:35:53 +0100

    buffer factory

--- HashMap.fs
+++ HashMap.fs
@@ -1,12 +1,6 @@
 module HashMap
 open System.Collections.Generic
 
-
-
-
-
-type 
-
 type HashMap<'K,'V  when 'K :> IEqualityComparer<'K> >() =
     let lBound = 0
     let hashAry = ResizeArray<KeyValueNode<'K,'V>>(0)
@@ -15,10 +9,52 @@ type HashMap<'K,'V  when 'K :> IEqualityComparer<'K> >() =
         let kvp = KeyValueNode(k,v)
         let hash = k.GetHashCode()
         let bucket = hash % (hashAry.Count)
-        if hash < lbound && hashAry.Count then 
+        if hash < lbound && hashAry.Count then
+        ()
 
-type HashMapFactory () =
+type Buffer<'K,'V when 'K :comparison>() =
 
-    member static New() =
+    let [<Literal>] ShardSize = 8
+    let buffer = ResizeArray<Map<'K,'V> []>(32)
 
-    member static 
+    let locks = ResizeArray<obj>(32)
+
+    let rental = ResizeArray<int>(32)
+
+    let mutable freeList : int list = []
+    //let mutable freeListCount = 0
+
+    member __.Rent() =
+        match freeList with
+        | [] ->
+            let newBuffer = Array.zeroCreate<Map<'K,'V>>(ShardSize) 
+            let index = buffer.Count
+            buffer.Add newBuffer
+            rental.Add 1
+            (index,newBuffer)
+        | h :: t -> 
+            freeList <- t
+            rental.[h] <- 1
+            (h,buffer.[h])
+
+    member __.Return(index:int , ary: Map<'K,'V> [] ) =
+        match rental.[index] with
+        | 0 -> failwith "A buffer as been returned when with rentals already zero"
+        | 1 ->    
+            System.Array.Clear(ary,0,ShardSize)
+            rental.[index] <- 0
+            freeList <- index :: freeList
+        | x ->
+            rental.[index] <- x - 1
+
+type BufferFactory() =
+    let buffers = Dictionary<System.Type,obj>()
+    member __.GetBuffer<'K,'V>() =
+        let t = typeof<'K * 'V>
+        match buffers.TryGetValue t with
+        | true, v -> v :?> Buffer<'K,'V> 
+        | false,_ ->
+            let newBuf = Buffer<'K,'V>()
+            buffers.Add(t, newBuf)
+            newBuf
+
